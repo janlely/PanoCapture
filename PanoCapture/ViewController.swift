3d70c12fe572064cde983c1dbe0e0c51097971f5
Jay <janlely@163.com>
2024-08-20 08:41:59 +0800

    create

---
+++ PanoCapture/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PanoCapture
+//
+//  Created by jin junjie on 2024/8/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
